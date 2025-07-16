45bccdb0d5c8e15913ebbc5c294bce2e689ada12
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-07-16 16:53:35 -0700

    feat: add MapFitBound and MapResizer components for dynamic map fitting and resizing

---
+++ src/components/Map/MapFitBound/index.tsx
@@ -0,0 +1,41 @@
+import { type FeatureCollection } from 'geojson';
+import L, { type LatLngExpression } from 'leaflet';
+import React, { useEffect } from 'react';
+import { useMap } from 'react-leaflet';
+
+interface MapFitBoundProps {
+  polygons: FeatureCollection[];
+  defaultLocation: LatLngExpression;
+  defaultZoom: number;
+}
+
+const MapFitBound: React.FC<MapFitBoundProps> = ({ polygons, defaultLocation, defaultZoom }) => {
+  const map = useMap();
+
+  /**
+   * Effect to set the map view when the component mounts or when the polygons change
+   * This is used to ensure that the map is centered on the polygons
+   * and that the map is resized to fit the polygons
+   * If no polygons are found, set the map to the default location
+   * If polygons are found, calculate the bounds to include all polygons
+   * and set the map view to the bounds
+   * If the bounds are not valid, set the map to the default location
+   */
+  useEffect(() => {
+    if (polygons.length > 0) {
+      // Calculate bounds to include all polygons
+      // This is to make sure the map resizes for the correct view
+      const bounds = L.featureGroup(polygons.map((geoJson) => L.geoJSON(geoJson))).getBounds();
+
+      if (bounds.isValid()) map.fitBounds(bounds, { padding: [20, 20] });
+      else map.setView(defaultLocation, defaultZoom, { animate: true });
+    } else {
+      // If no polygons are found, set the map to the default location
+      map.setView(defaultLocation, defaultZoom, { animate: true });
+    }
+  }, [polygons, map, defaultLocation, defaultZoom]);
+
+  return null;
+};
+
+export default MapFitBound;
---
+++ src/components/Map/MapLayout/index.tsx
@@ -0,0 +1,76 @@
+import { type FC, useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet';
+
+import MapFitBound from '@/components/Map/MapFitBound';
+
+import 'leaflet/dist/leaflet.css';
+import './index.scss';
+
+interface MapLayoutProps {
+  position?: [number, number];
+  zoomLevel?: number;
+}
+
+const MapLayout: FC<MapLayoutProps> = ({ position = [48.43737, -123.35883], zoomLevel = 13 }) => {
+  const [mapCenter, setMapCenter] = useState<[number, number]>(position);
+  const [mapZoom, setMapZoom] = useState<number>(zoomLevel);
+
+  useEffect(() => {
+    setMapCenter(position);
+  }, [position]);
+
+  useEffect(() => {
+    setMapZoom(zoomLevel);
+  }, [zoomLevel]);
+
+  return (
+    <div className="map-layout">
+      <MapContainer
+        center={mapCenter}
+        zoom={mapZoom}
+        scrollWheelZoom={false}
+        className="leaflet-container"
+      >
+        <MapFitBound polygons={[]} defaultLocation={mapCenter} defaultZoom={mapZoom} />
+
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer name="ESRI Topography">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+              zIndex={-10000}
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="ESRI Satellite">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+              zIndex={-10000}
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="OpenTopoMap">
+            <TileLayer
+              url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+              attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+              zIndex={-10000}
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer checked name="OpenStreets">
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              zIndex={-10000}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+        <Marker position={mapCenter}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapLayout;
