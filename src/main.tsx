45726eb743b80f96f3cd818a7d87ba30fb015a49
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-07-16 10:41:49 -0700

    chore: update dependencies and restructure styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './main.scss';
 import App from './App.tsx';
+
+import '@/styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variables.scss";`,
-      },
-    },
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
-  },
 });
