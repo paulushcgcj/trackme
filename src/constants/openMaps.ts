68e4bc74fd62a48c96670fe44affe521052005ba
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-07-18 10:58:51 -0700

    feat: add temporary OpenMaps API URL and parameters configuration

---
+++ src/constants/openMaps.ts
@@ -0,0 +1,24 @@
+//TODO: This entire file is a temporary fix for the OpenMaps API URL.
+// It should be replaced with a more robust solution in the future.
+const BASE_URL = 'https://openmaps.gov.bc.ca/geo/ows';
+
+const params: Record<string, string> = {
+  service: 'WFS',
+  version: '2.0.0',
+  request: 'GetFeature',
+  outputFormat: 'application/json',
+  SrsName: 'EPSG:4326',
+  PROPERTYNAME: 'OPENING_ID,GEOMETRY',
+  //cql_filter: 'OPENING_ID=1725701',
+  cql_filter: 'OPENING_ID=1771831',
+  typeName: 'WHSE_FOREST_TENURE.FTEN_CUT_BLOCK_POLY_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_STANDARDS_UNIT_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_ACTIVITY_TREATMENT_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_PLANTING_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_INV_SVW,WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_RESERVE_SVW,WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_SILV_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_RESERVE_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_SILV_SVW',
+  //typeName: 'WHSE_FOREST_VEGETATION.RSLT_FOREST_COVER_INV_SVW',
+};
+
+export const OPEN_MAPS_URL = `${BASE_URL}?${new URLSearchParams(params).toString()}`;
